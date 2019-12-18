525a6b220eb905ded27c630409595cc5b05edcac
sureshui1990 <sureshui@gmail.com>
2019-12-18 21:00:22 +0530

    Functionality of an emit event and send it to all user

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const app = require('express')();
 const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 const port = 5000;
 
 app.get('/', (req,res) => {
     res.sendFile(__dirname+'/index.html');
-})
+});
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+    socket.on('chat message', (message) => {
+        io.emit('chat message', message);
+    })
+    io.on('disconnection', () => (console.log('User disconnected')));
+});
 
 http.listen(port, () => (console.log(`Listening on * ${port}`)));
